7d50022eaa4cb1ab848bcfa42db5fc4df4c0801d
Fabian <fabrofabian@gmail.com>
2025-08-06 01:38:18 -0700

    refactored PATCH request into the other route file

--- src/app/api/weapons/[id]/route.ts
+++ src/app/api/weapons/[id]/route.ts
@@ -2,6 +2,31 @@ import { NextResponse, NextRequest } from "next/server";
 import { db } from '@/lib/db';
 import { ResultSetHeader } from "mysql2";
 
+export async function PATCH(req: NextRequest, {params }: { params: { id: string}}) {
+    const weapon_id = params.id;
+    const body = await req.json();
+    const { w_name, damage } = body;
+
+    if(!weapon_id) {
+        return NextResponse.json({ error: 'Missing weapon ID'}, {status: 400});
+    }
+
+    try {
+        if(w_name !== undefined && w_name !== "") {
+            await db.query('UPDATE weapons SET w_name = ? WHERE weapon_id = ?', [w_name, weapon_id]);
+        }
+
+        if(damage !== undefined && damage !== "") {
+            await db.query('UPDATE weapons SET damage = ? WHERE weapon_id = ?', [damage, weapon_id]);
+        }
+
+        return NextResponse.json({ success: true});
+    } catch(err) {
+        console.error(err);
+        return NextResponse.json({ error: 'Database update failed'} , {status: 500});
+    }
+}
+
 export async function DELETE(req: NextRequest, { params } :  {params: { id: string}}){
 
     const id = params.id;
